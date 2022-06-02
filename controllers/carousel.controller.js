9b5b3f2185a4728904ae72b12e61befb806457f3
AngelinaCP <89658366303@mail.ru>
2022-06-02 20:24:39 +0300

    changed password

--- controllers/carousel.controller.js
+++ controllers/carousel.controller.js
@@ -3,13 +3,6 @@ const db = require('../db')
 
 class carouselController {
 	async getAllSpecPosts(req, res) {
-		// try {
-		// 	const allCars = await db.query("SELECT * FROM cars; SELECT url, alt from photo join cars on car_id = cars.id where photo.car_id = cars.id;");
-		// 	res.json(allCars.rows);
-		//   } catch (err) {
-		// 	console.error(err.message);
-		//   }
-
 		const allCars = await db.query("SELECT spec_card, city, marka, model, engine, year, price, transmission, drive_unit, body_type, color, mileage, \
 										json_agg(photo.*) as PHOTO \
 										FROM cars JOIN photo ON photo.car_id = cars.id \
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Coldplay",
+  password: "admin",
   host: "localhost",
   port: 5432,
   database: "drom3"
